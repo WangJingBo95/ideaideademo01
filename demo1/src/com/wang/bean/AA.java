6b18022443fb7f6abcd0858bfe4d73583be3b517
wangjingbo <657941428@qq.com>
2020-11-23 23:52:49 +0800

    Initial commit

---
+++ demo1/src/com/wang/bean/AA.java
@@ -0,0 +1,12 @@
+package com.wang.bean;
+
+/**
+ * @author wangjingbo
+ * @date 2020-11-22 22:16
+ */
+public class AA {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("AA.main");
+    }
+}
---
+++ src/com/wang/bean/Customer.java
@@ -0,0 +1,21 @@
+package com.wang.bean;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        ArrayList<String> strings = new ArrayList<>();
+    }
+
+    public void method() {
+        AA aa = new AA();
+    }
+
+    public void testUpdate() {
+        String a = "";
+
+    }
+
+}
